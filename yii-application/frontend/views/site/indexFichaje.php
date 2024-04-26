31a206a9d324e95b777edfc979de0395bae54385
Javier Cerda <javier.cerda@itevebasa.com>
2024-04-26 11:59:12 +0200

    yii\bootstrap\ActiveForm modificado a yii\widgets\ActiveForm

--- yii-application/frontend/views/site/indexFichaje.php
+++ yii-application/frontend/views/site/indexFichaje.php
@@ -1,7 +1,7 @@
 <?php
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
-use yii\bootstrap\ActiveForm;
+use yii\widgets\ActiveForm;
 
 use common\models\Accion;
 
--- yii-application/frontend/views/site/login.php
+++ yii-application/frontend/views/site/login.php
@@ -5,7 +5,7 @@
 /* @var $model \common\models\LoginForm */
 
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+use yii\widgets\ActiveForm;
 
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
